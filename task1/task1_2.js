c8f0404c795bf4afedd0c7c6a7244aa4e2e2ae2f
Oleksandr Fedorov <aleksandr.fedorov@gmail.com>
2020-02-10 18:46:15 +0200

    Task 1.2: refactoring to use pipeline

--- task1/task1_2.js
+++ task1/task1_2.js
@@ -1,22 +1,22 @@
 const readFileName = 'task1/csv/nodejs-hw1-ex1.csv';
 const writeFileName = 'task1/csv/nodejs-hw1-ex1.txt';
 
+const { pipeline } = require('stream');
 const fs = require('fs');
-const { promisify } = require('util');
-const appendFileAsync = promisify(fs.appendFile);
+const csv = require('csvtojson');
 
-const csvConverter = require('csvtojson') ({
-    ignoreColumns: /Amount/,
-    headers: ['book', 'author', 'price']
-});
-
-function csvToJson() {
-    csvConverter
-        .fromStream(fs.createReadStream(readFileName))
-        .on('error', console.error)
-        .subscribe(jsonObj => {
-            return appendFileAsync(writeFileName, JSON.stringify(jsonObj) + '\n');
-        }, console.error);
-}
-
-fs.writeFile(writeFileName, '', csvToJson);
+pipeline(
+    fs.createReadStream(readFileName),
+    csv({
+        headers: ['book', 'author', 'amount', 'price'],
+        ignoreColumns: /amount/
+    }),
+    fs.createWriteStream(writeFileName),
+    (err) => {
+        if (err) {
+            console.error('Pipeline failed.', err);
+        } else {
+            console.log('Pipeline succeeded.');
+        }
+    }
+);
