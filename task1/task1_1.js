c258864bd591eda169a4d60b1554e489a9706b26
Oleksandr Fedorov <aleksandr.fedorov@gmail.com>
2020-02-23 21:19:59 +0200

    Task 1.1: Fix review comments

--- task1/task1_1.js
+++ task1/task1_1.js
@@ -1,20 +1,23 @@
-const Emitter = require('events');
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
 
-function lineEmitter() {
-    const emitter = new Emitter();
-
-    let buffer = '';
-    process.stdin.setEncoding('utf8');
-    process.stdin.on('data', (data) => {
-        buffer += data;
-        const lines = buffer.split(/[\r?\n]/);
-        buffer = lines.pop();
-        lines.forEach(line => emitter.emit('line', line));
-    });
-
-    return emitter;
+function lineListener(line) {
+    process.stdout.write(reverseLine(line) + '\n');
 }
 
-lineEmitter().on('line', line => {
-    process.stdout.write(line.split("").reverse().join("") + '\n');
+function reverseLine(line) {
+    return line.split("").reverse().join("");
+}
+
+function emitLine(line) {
+    eventEmitter.emit('line', line);
+}
+
+eventEmitter.on('line', lineListener);
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', (data) => {
+    const lines = data.trim().split(/[\r?\n]/);
+    lines.forEach(emitLine);
 });
+
