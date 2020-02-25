2211def80d5a219b37c30d70404b6bdd227c65bb
Oleksandr Fedorov <aleksandr.fedorov@gmail.com>
2020-02-25 19:50:00 +0200

    Task 2.2: Validation

--- src/task2/task2.js
+++ src/task2/task2.js
@@ -1,8 +1,25 @@
 import express from 'express';
+import Joi from 'joi';
 import userRepository from './userRepository';
 
 const app = express();
 const router = express.Router();
+
+const optionalLoginRule = Joi.string().alphanum().min(3).max(30);
+const optionalPasswordRule = Joi.string().regex(/^(?=.*[a-zA-Z])(?=.*[0-9]).*$/);
+const optionalAgeRule = Joi.number().min(4).max(130);
+
+const postUserSchema = Joi.object().keys({
+    login: optionalLoginRule.required(),
+    password: optionalPasswordRule.required(),
+    age: optionalAgeRule.required()
+});
+
+const putUserSchema = Joi.object().keys({
+    login: optionalLoginRule,
+    password: optionalPasswordRule,
+    age: optionalAgeRule
+});
 
 app.listen(3000);
 app.use(express.json());
@@ -17,6 +34,7 @@ router.route('/users/:id')
     .get((req, res) => {
         res.json(req.user);
     })
+    .put(validateSchema(putUserSchema))
     .put((req, res) => {
         const updatedUser = userRepository.updateUser(req.user.id, req.body);
         res.json(updatedUser);
@@ -36,7 +54,35 @@ router.route('/users')
             res.json(userRepository.getAutoSuggestUsers(loginSubstring, Number.parseInt(limit, 10)));
         }
     })
+    .post(validateSchema(postUserSchema))
     .post((req, res) => {
         const storedUser = userRepository.createUser(req.body);
         res.json(storedUser);
     });
+
+function errorResponse(schemaErrors) {
+    const errors = schemaErrors.map(error => {
+        const { path, message } = error;
+        return { path, message };
+    });
+
+    return {
+        status: 'failed',
+        errors
+    };
+}
+
+function validateSchema(schema) {
+    return (req, res, next) => {
+        const { error } = schema.validate(req.body, {
+            abortEarly: false,
+            allowUnknown: false
+        });
+
+        if (error && error.isJoi) {
+            res.status(400).json(errorResponse(error.details));
+        } else {
+            return next();
+        }
+    };
+}
