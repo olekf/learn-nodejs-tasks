7f021b22847702ab5b0730c5ec9ad647e9795017
Oleksandr Fedorov <aleksandr.fedorov@gmail.com>
2020-03-06 13:16:25 +0200

    Task 2.2 Fix review comments

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,24 +1,8 @@
 import express from 'express';
 import { userRepository } from "../models/user";
-import Joi from 'joi';
+import { validatePostUserSchema, validatePutUserSchema } from "../routes/validation";
 
 export const userRouter = express.Router();
-
-const optionalLoginRule = Joi.string().alphanum().min(3).max(30);
-const optionalPasswordRule = Joi.string().regex(/^(?=.*[a-zA-Z])(?=.*[0-9]).*$/);
-const optionalAgeRule = Joi.number().min(4).max(130);
-
-const postUserSchema = Joi.object().keys({
-    login: optionalLoginRule.required(),
-    password: optionalPasswordRule.required(),
-    age: optionalAgeRule.required()
-});
-
-const putUserSchema = Joi.object().keys({
-    login: optionalLoginRule,
-    password: optionalPasswordRule,
-    age: optionalAgeRule
-});
 
 userRouter.param('id', (req, res, next, id) => {
     req.user = userRepository.getUserById(id);
@@ -29,7 +13,7 @@ userRouter.route('/:id')
     .get((req, res) => {
         res.json(req.user);
     })
-    .put(validateSchema(putUserSchema))
+    .put(validatePostUserSchema())
     .put((req, res) => {
         const updatedUser = userRepository.updateUser(req.user.id, req.body);
         res.json(updatedUser);
@@ -49,35 +33,8 @@ userRouter.route('/')
             res.json(userRepository.getAutoSuggestUsers(loginSubstring, Number.parseInt(limit, 10)));
         }
     })
-    .post(validateSchema(postUserSchema))
+    .post(validatePutUserSchema())
     .post((req, res) => {
         const storedUser = userRepository.createUser(req.body);
         res.json(storedUser);
     });
-
-function errorResponse(schemaErrors) {
-    const errors = schemaErrors.map(error => {
-        const { path, message } = error;
-        return { path, message };
-    });
-
-    return {
-        status: 'failed',
-        errors
-    };
-}
-
-function validateSchema(schema) {
-    return (req, res, next) => {
-        const { error } = schema.validate(req.body, {
-            abortEarly: false,
-            allowUnknown: false
-        });
-
-        if (error && error.isJoi) {
-            res.status(400).json(errorResponse(error.details));
-        } else {
-            return next();
-        }
-    };
-}
