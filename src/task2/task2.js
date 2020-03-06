bd432a5f610b6e4d0d7ca8576d9281f57d7a86dd
Oleksandr Fedorov <aleksandr.fedorov@gmail.com>
2020-03-06 12:09:32 +0200

    Task 2.1 Fix review comments

--- src/task2/task2.js
+++ src/task2/task2.js
@@ -1,42 +1,8 @@
 import express from 'express';
-import userRepository from './userRepository';
+import { userRouter } from '../routes/user';
 
 const app = express();
-const router = express.Router();
 
 app.listen(3000);
 app.use(express.json());
-app.use('/', router);
-
-router.param('id', (req, res, next, id) => {
-    req.user = userRepository.getUserById(id);
-    return next();
-});
-
-router.route('/users/:id')
-    .get((req, res) => {
-        res.json(req.user);
-    })
-    .put((req, res) => {
-        const updatedUser = userRepository.updateUser(req.user.id, req.body);
-        res.json(updatedUser);
-    })
-    .delete((req, res) => {
-        userRepository.removeUser(req.user.id);
-        res.sendStatus(200);
-    });
-
-router.route('/users')
-    .get((req, res) => {
-        const loginSubstring = req.query.loginSubstring;
-        const limit = req.query.limit;
-        if (loginSubstring === undefined || limit === undefined) {
-            res.json(userRepository.getAllUsers());
-        } else {
-            res.json(userRepository.getAutoSuggestUsers(loginSubstring, Number.parseInt(limit, 10)));
-        }
-    })
-    .post((req, res) => {
-        const storedUser = userRepository.createUser(req.body);
-        res.json(storedUser);
-    });
+app.use('/users', userRouter);
