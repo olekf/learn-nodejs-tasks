c97616245b1233b698173cc5c17987bfdbe7632f
Oleksandr Fedorov <aleksandr.fedorov@gmail.com>
2020-03-22 23:53:53 +0200

    Task 4.2 CORS

--- src/app/app.js
+++ src/app/app.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import { userRouter } from '../routes/user';
 import { rootRouter } from '../routes/root';
 import { logger } from '../config/winston';
+import { corsOptions } from '../config/cors';
 
 const app = express();
 
 app.listen(3000);
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(morgan(':method :url'));
 app.use('/', rootRouter);
 app.use('/users', userRouter);
